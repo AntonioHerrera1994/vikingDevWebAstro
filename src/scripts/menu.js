4a30ad55588e96eb8c8941e2887f43358feb3279
Antonio Herrera <herreraquinteroj@gmail.com>
2024-09-20 15:00:47 -0700

    commit septiembre 20

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,32 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-  });
+  document.querySelector('.nav-links').classList.toggle('expanded');
+});
+
+document.querySelector('.close').addEventListener('click',   
+() => {
+document.querySelector('.nav-links').classList.remove('expanded');
+});
+
+
+window.addEventListener('scroll', function() {
+  const div = document.getElementById('miDiv');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) { // Cambia a 100px o la distancia que prefieras
+    div.style.backgroundColor = 'white'; // Nuevo color de fondo
+  } else {
+    div.style.backgroundColor = '#ffffff00'; // Color inicial
+  }
+});
+
+window.addEventListener('scroll', function() {
+  const div = document.getElementById('miMenu');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) { // Cambia a 100px o la distancia que prefieras
+    div.style.backgroundColor = 'white'; // Nuevo color de fondo
+  } else {
+    div.style.backgroundColor = 'white'; // Color inicial
+  }
+});
+
