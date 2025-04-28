c84eb07e9c20f670d551e81a4642af356f99e33f
Antonio Herrera <herreraquinteroj@gmail.com>
2025-04-28 13:35:49 -0700

    cambios-SEO

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,13 +1,24 @@
+// Abrir menú
+
 document.querySelector('.hamburger').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('expanded');
 });
 
+// Cerrar menú al hacer clic en el botón "close"
 document.querySelector('.close').addEventListener('click',   
 () => {
 document.querySelector('.nav-links').classList.remove('expanded');
 });
 
+// Cerrar menú al hacer clic en cualquier enlace dentro de nav-links
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.remove('expanded');
+  });
+});
 
+
+// Cambiar color de fondo al hacer scroll (para #miDiv)
 window.addEventListener('scroll', function() {
   const div = document.getElementById('miDiv');
   const scrollPosition = window.scrollY;
@@ -19,6 +30,8 @@ window.addEventListener('scroll', function() {
   }
 });
 
+
+// Cambiar color de fondo al hacer scroll (para #miMenu)
 window.addEventListener('scroll', function() {
   const div = document.getElementById('miMenu');
   const scrollPosition = window.scrollY;
