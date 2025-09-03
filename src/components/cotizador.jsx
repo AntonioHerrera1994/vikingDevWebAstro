4e4d4a7c1e7b810683a43ab8806d80b226fd1f3e
Antonio Herrera <herreraquinteroj@gmail.com>
2025-09-03 09:37:39 -0700

    portafolio

--- src/components/cotizador.jsx
+++ src/components/cotizador.jsx
@@ -12,8 +12,8 @@ export default function Cotizacion() {
   const preciosTipoSitio = {
     "no necesito sitio web": 0,
     "sitio one page": 4000,
-    "sitio web completo": 5500,
-    "e-commerce": 7500,
+    "sitio web completo": 5000,
+    "e-commerce": 7000,
   };
 
   const preciosServiciosAdicionales = {
