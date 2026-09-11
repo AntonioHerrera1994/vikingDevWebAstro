2c5dec0e3ac5960ef2c1fe816d4d5e7a5b86b8d8
Antonio Herrera <herreraquinteroj@gmail.com>
2026-09-11 12:47:24 -0700

    imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vikingdevtj.com/',
   integrations: [react(), sitemap()],
-  adapter: netlify()
+  adapter: netlify({
+    devFeatures: {
+      edgeFunctions: false,
+    },
+  }),
 });
